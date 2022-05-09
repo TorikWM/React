50c03054527a881dbd79d05fb9771cce224896f3
E-Dev <81751310+TorikWM@users.noreply.github.com>
2022-05-09 18:12:16 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button , Form , Navbar , Container , Nav} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div>
+<Navbar bg="dark" variant="dark">
+    <Container>
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar>
+
+      <Form>
+  <fieldset disabled>
+    <Form.Group className="mb-3">
+      <Form.Label htmlFor="disabledTextInput">Disabled input</Form.Label>
+      <Form.Control id="disabledTextInput" placeholder="Disabled input" />
+    </Form.Group>
+    <Form.Group className="mb-3">
+      <Form.Label htmlFor="disabledSelect">Disabled select menu</Form.Label>
+      <Form.Select id="disabledSelect">
+        <option>Disabled select</option>
+      </Form.Select>
+    </Form.Group>
+    <Form.Group className="mb-3">
+      <Form.Check
+        type="checkbox"
+        id="disabledFieldsetCheck"
+        label="Can't check this"
+      />
+    </Form.Group>
+    <Button type="submit">Submit</Button>
+  </fieldset>
+</Form>
+
+<Button variant="danger">Start</Button>
+    </div>
+  );
+}
+
+export default App;
